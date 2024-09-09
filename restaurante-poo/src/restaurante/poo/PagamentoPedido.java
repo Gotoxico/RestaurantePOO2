8c2c97d7e59def6cf092dc9b6670ffb3404bd206
yasmin <142116724+yasminbonett@users.noreply.github.com>
2024-09-09 20:40:57 -0300

    Create PagamentoPedido.java

---
+++ restaurante-poo/src/restaurante/poo/PagamentoPedido.java
@@ -0,0 +1,71 @@
+package restaurante.poo;
+
+import java.util.UUID;
+
+public class PagamentoPedido {
+    private String idPagamento;
+    private Pedido pedido;
+    private double valorTotal;
+    private boolean pago;
+    private String formaPagamento;
+
+    public PagamentoPedido(Pedido pedido, String formaPagamento){
+        this.idPagamento = UUID.randomUUID().toString();
+        this.pedido = pedido;
+        this.formaPagamento = formaPagamento;
+        this.valorTotal = calcularTotalPedido();
+        this.pago = false;
+    }
+
+    private double calcularTotalPedido() {
+        double total = 0.0;
+        for(ItemMenu item : pedido.getItensPedido()){
+            total += item.getPreco() * item.getQuantidade();
+        }
+        return total;
+    }
+
+    public boolean realizarPagamento(){
+        if(!pago){
+            this.pago = true;
+            System.out.println("Pagamento de R$" + valorTotal + " realizado com sucesso.");
+            return true;
+        }else{
+            System.out.println("O pagamento já foi realizado.");
+            return false;
+        }
+    }
+
+    public boolean estaPago(){
+        return pago;
+    }
+
+    public String getIdPagamento(){
+        return idPagamento;
+    }
+
+    public Pedido getPedido(){
+        return pedido;
+    }
+
+    public double getValorTotal(){
+        return valorTotal;
+    }
+
+    public String getFormaPagamento(){
+        return formaPagamento;
+    }
+
+    public void setFormaPagamento(String formaPagamento){
+        this.formaPagamento = formaPagamento;
+    }
+
+    @Override
+    public String toString(){
+        return "PagamentoPedido:\n" +
+               "idPagamento = " + idPagamento + "\n" +
+               "Valor Total = " + valorTotal + "\n" +
+               "Pago = " + pago + "\n" +
+               "Forma de Pagamento = " + formaPagamento + "\n";
+    }
+}
